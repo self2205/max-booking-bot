508582c9ff9f1addacb4221f03b24bb5cc85652a
self2205 <vladborkin1996@gmail.com>
2026-07-01 13:47:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Booking(BaseModel):
+    product: str
+    name: str
+    phone: str
+
 
 @app.get("/")
 def root():
     return {
         "status": "ok",
         "service": "MAX Booking Bot"
+    }
+
+
+@app.post("/booking")
+def booking(data: Booking):
+    print("====== НОВАЯ ЗАЯВКА ======")
+    print(f"Товар: {data.product}")
+    print(f"Имя: {data.name}")
+    print(f"Телефон: {data.phone}")
+    print("==========================")
+
+    return {
+        "success": True
     }
