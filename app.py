efc101d1085ae96b24194695d76710ca270d59ad
self2205 <vladborkin1996@gmail.com>
2026-07-01 14:05:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,3 +37,35 @@ async def webhook(request: Request):
     print("=================================")
 
     return {"ok": True}
+import os
+import requests
+
+MAX_TOKEN = os.getenv("MAX_TOKEN")
+MAX_SECRET = os.getenv("MAX_SECRET")
+
+
+@app.get("/setup")
+def setup():
+
+    headers = {
+        "Authorization": MAX_TOKEN,
+        "Content-Type": "application/json"
+    }
+
+    body = {
+        "url": "https://max-booking-bot-k3dx.onrender.com/webhook",
+        "update_types": [
+            "message_created",
+            "bot_added",
+            "bot_started"
+        ],
+        "secret": MAX_SECRET
+    }
+
+    r = requests.post(
+        "https://platform-api2.max.ru/subscriptions",
+        headers=headers,
+        json=body
+    )
+
+    return r.json()
