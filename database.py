91bf22d11e53e97d6e9f36b0d172b578131c7ff7
self2205 <vladborkin1996@gmail.com>
2026-07-01 15:56:35 +0300

    Update database.py

--- database.py
+++ database.py
@@ -41,3 +41,48 @@ def save_booking(product, name, phone):
 
     conn.commit()
     conn.close()
+    def change_status(booking_id):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT status FROM bookings WHERE id=?",
+        (booking_id,)
+    )
+
+    row = cursor.fetchone()
+
+    if not row:
+        conn.close()
+        return
+
+    status = row[0]
+
+    if status == "🟢 Новая":
+        new_status = "🟡 В работе"
+    elif status == "🟡 В работе":
+        new_status = "✅ Выполнена"
+    else:
+        new_status = "🟢 Новая"
+
+    cursor.execute(
+        "UPDATE bookings SET status=? WHERE id=?",
+        (new_status, booking_id)
+    )
+
+    conn.commit()
+    conn.close()
+    def get_bookings():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, product, name, phone, status, created_at
+        FROM bookings
+        ORDER BY id DESC
+    """)
+
+    rows = cursor.fetchall()
+    conn.close()
+
+    return rows
