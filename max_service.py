0686aa9156e6620dcd6d3fc5bdd6325e996feb79
self2205 <vladborkin1996@gmail.com>
2026-07-02 16:46:26 +0300

    Update max_service.py

--- max_service.py
+++ max_service.py
@@ -1,23 +1,11 @@
 import requests
-
-
 from config import MAX_TOKEN
 
-
-def send_message_max(data, text):
-    """
-    Отправка сообщения пользователю в MAX
-    """
-
+def send_message_max(data, text: str):
     try:
         message = data.get("message", {})
         recipient = message.get("recipient", {})
-
         chat_id = recipient.get("chat_id")
-
-        if not chat_id:
-            print("MAX ERROR: chat_id not found")
-            return
 
         url = f"https://platform-api2.max.ru/messages?chat_id={chat_id}"
 
@@ -38,12 +26,8 @@ def send_message_max(data, text):
             timeout=10
         )
 
-        print("========== SEND TO MAX ==========")
-        print("URL:", url)
-        print("Payload:", payload)
-        print("Status:", response.status_code)
-        print("Response:", response.text)
-        print("=================================")
+        print("MAX STATUS:", response.status_code)
+        print("MAX RESPONSE:", response.text)
 
     except Exception as e:
         print("MAX ERROR:", e)
