69364d66b693216dad1eafb3491926ae313248ab
self2205 <vladborkin1996@gmail.com>
2026-07-02 12:53:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,13 +54,20 @@ def send_to_telegram(product, name, phone):
 MAX_TOKEN = "REMOVED_SECRET"
 
 
-def send_message_max(user_id: int, text: str):
+def send_message_max(data, text: str):
     try:
         url = "https://platform-api2.max.ru/messages"
 
+        message = data.get("message", {})
+        recipient = message.get("recipient", {})
+
+        chat_id = recipient.get("chat_id")
+        chat_type = recipient.get("chat_type")
+
         payload = {
             "recipient": {
-                "user_id": user_id
+                "chat_id": chat_id,
+                "chat_type": chat_type
             },
             "text": text
         }
