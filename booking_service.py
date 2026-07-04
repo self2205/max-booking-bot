7123ddd07025986f64f53ce09284a8d304494184
self2205 <vladborkin1996@gmail.com>
2026-07-04 09:41:59 +0300

    Update booking_service.py

--- booking_service.py
+++ booking_service.py
@@ -2,12 +2,14 @@ from database import save_booking
 from telegram_service import send_to_telegram
 
 
-def create_booking(product: str, name: str, phone: str):
+def create_booking(product, name, phone, image_url=None):
     """
     Создает заявку:
     - сохраняет в PostgreSQL
     - отправляет в Telegram
     """
+
+    print("IMAGE IN BOOKING:", image_url)
 
     booking_id = save_booking(
         product=product,
@@ -18,7 +20,8 @@ def create_booking(product: str, name: str, phone: str):
     send_to_telegram(
         product=product,
         name=name,
-        phone=phone
+        phone=phone,
+        image_url=image_url
     )
 
     print("\n========== НОВАЯ ЗАЯВКА ==========")
@@ -26,6 +29,7 @@ def create_booking(product: str, name: str, phone: str):
     print(f"Товар: {product}")
     print(f"Имя: {name}")
     print(f"Телефон: {phone}")
+    print(f"Фото: {image_url}")
     print("=================================\n")
 
     return booking_id
