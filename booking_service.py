7bafa5f2c935520fa40a7c97e8e5a13fcc076a47
self2205 <vladborkin1996@gmail.com>
2026-07-07 15:59:18 +0300

    Update booking_service.py

--- booking_service.py
+++ booking_service.py
@@ -1,34 +1,129 @@
-from database import save_booking
+from database import (
+    save_booking,
+    get_product
+)
+
 from telegram_service import send_to_telegram
 
 
-def create_booking(product, name, phone, image_url=None):
+
+def create_booking(
+        product,
+        name,
+        phone,
+        image_url=None
+):
     """
     Создает заявку:
     - сохраняет в PostgreSQL
-    - отправляет в Telegram
+    - отправляет в Telegram админам с фото из канала
     """
 
+
+
+    # ==========================
+    # ИЩЕМ ПОСТ В КАНАЛЕ
+    # ==========================
+
+    channel_message_id = None
+
+
+    try:
+
+        rows = get_product_by_name(product)
+
+
+        if rows:
+
+            channel_message_id = rows.get(
+                "channel_message_id"
+            )
+
+
+    except Exception as e:
+
+        print(
+            "GET CHANNEL MESSAGE ERROR:",
+            e
+        )
+
+
+
+
+
+    # ==========================
+    # СОХРАНЯЕМ ЗАЯВКУ
+    # ==========================
+
     booking_id = save_booking(
+
         product=product,
+
         name=name,
+
         phone=phone,
+
         image_url=image_url
+
     )
+
+
+
+
+
+    # ==========================
+    # ОТПРАВЛЯЕМ АДМИНАМ
+    # ==========================
 
     send_to_telegram(
+
         product=product,
+
         name=name,
+
         phone=phone,
-        image_url=image_url
+
+        image_url=image_url,
+
+        channel_message_id=channel_message_id
+
     )
 
-    print("\n========== НОВАЯ ЗАЯВКА ==========")
-    print(f"ID: {booking_id}")
-    print(f"Товар: {product}")
-    print(f"Имя: {name}")
-    print(f"Телефон: {phone}")
-    print(f"Фото: {image_url}")
-    print("=================================\n")
+
+
+
+
+    print(
+        "\n========== НОВАЯ ЗАЯВКА =========="
+    )
+
+    print(
+        f"ID: {booking_id}"
+    )
+
+    print(
+        f"Товар: {product}"
+    )
+
+    print(
+        f"Имя: {name}"
+    )
+
+    print(
+        f"Телефон: {phone}"
+    )
+
+    print(
+        f"Фото: {image_url}"
+    )
+
+    print(
+        f"CHANNEL MESSAGE ID: {channel_message_id}"
+    )
+
+    print(
+        "=================================\n"
+    )
+
 
     return booking_id
